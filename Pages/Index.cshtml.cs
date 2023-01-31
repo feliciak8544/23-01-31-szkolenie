173b4c62748cfbae3c090f077534615b87b6eb57
feliciak8544 <121878044+feliciak8544@users.noreply.github.com>
2023-01-31 10:36:13 +0000

    razor

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _23_01_31_szkolenie.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
